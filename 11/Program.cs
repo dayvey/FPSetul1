e12912676f3816c434553a52af69b5a40593d59f
dayvey <115526062+dayvey@users.noreply.github.com>
2022-11-30 11:46:59 +0200

    Add files via upload

---
+++ 11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, m = 0;
+
+            Console.Write("n=");
+            n = int.Parse(Console.ReadLine());
+
+            int l = n;
+
+            while (n>0)
+            {
+                m = m * 10 + n % 10;
+                n /= 10;
+            }
+
+            Console.WriteLine("{0} cu cifrele in ordine inversa: {1}", l, m);
+        }
+    }
+}
---
+++ 12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, a, b;
+
+            Console.Write("n=");
+            n = int.Parse(Console.ReadLine());
+
+            Console.Write("a=");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("b=");
+            b = int.Parse(Console.ReadLine());
+
+            int nr = 0;
+
+            for (int i=a; i<=b; i++)
+            {
+                if (i%n==0)
+                    nr++;
+            }
+
+            Console.WriteLine("Intre {0} si {1} sunt {2} numere divizibile cu {3}", a, b, nr, n);
+        }
+    }
+}
