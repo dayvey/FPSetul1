7e6c8436b9a37248b8d80143751df5acc08630aa
dayvey <115526062+dayvey@users.noreply.github.com>
2022-11-30 11:43:06 +0200

    Add files via upload

---
+++ 5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, k;
+
+            Console.Write("n=");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("k=");
+            k = Convert.ToInt32(Console.ReadLine());
+
+            int l = k;
+            int m = n;
+
+            while (k > 1)
+            {
+                k--;
+                n /= 10;
+            }
+
+            k = n % 10;
+
+            Console.WriteLine("a {0}-a cifra din {1} de la dreapta la stanga este {2}", l, m, k);
+        }
+    }
+}
---
+++ 6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b, c;
+
+            Console.Write("a=");
+            a = float.Parse(Console.ReadLine());
+
+            Console.Write("b=");
+            b = float.Parse(Console.ReadLine());
+            
+            Console.Write("c=");
+            c = float.Parse(Console.ReadLine());
+
+            if (a < b + c && b < a + c && c < a + b)
+                Console.WriteLine("{0}, {1} si {2} pot fi laturile unui triunghi!", a, b, c);
+            else Console.WriteLine("{0}, {1} si {2} nu pot fi laturile unui triunghi!", a, b, c);
+        }
+    }
+}
