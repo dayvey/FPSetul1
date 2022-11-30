0de5af4f8f4c9313e725badc7c020c9e5e638f1f
dayvey <115526062+dayvey@users.noreply.github.com>
2022-11-30 11:45:57 +0200

    Add files via upload

---
+++ 10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, c=0;
+
+            Console.Write("n=");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 2; i <= n/2 && c==0; i++)
+            {
+                if (n % i == 0)
+                    c = 1;
+            }
+
+            if (c == 0)
+                Console.WriteLine("{0} este prim", n);
+            else Console.WriteLine("{0} nu este prim", n);
+        }
+    }
+}
---
+++ 9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.Write("n=");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            int c = 0;
+
+            for (int i = 2; i <= n/2; i++)
+            {
+                if (n % i == 0)
+                    Console.Write("{0} ", i);
+            }
+        }
+    }
+}
