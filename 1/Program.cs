2ee6951075207225d0b47a84d630c3e50529ccda
dayvey <115526062+dayvey@users.noreply.github.com>
2022-11-30 11:38:28 +0200

    Add files via upload

---
+++ 1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            float a, b;
+
+            Console.Write("a=");
+            a=float.Parse(Console.ReadLine());
+
+            Console.Write("b=");
+            b = float.Parse(Console.ReadLine());
+
+            b = -b;
+            b /= a;
+
+            Console.WriteLine("x={0}", b);
+        }
+    }
+}
---
+++ 2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b, c;
+
+            Console.Write("a=");
+            a=float.Parse(Console.ReadLine());
+
+            Console.Write("b=");
+            b = float.Parse(Console.ReadLine());
+
+            Console.Write("c=");
+            c = float.Parse(Console.ReadLine());
+
+            float delta = (b * b) - (4 * a * c);
+
+            if (delta > 0)
+            {
+                float x1, x2;
+                x1 = ( (float)Math.Sqrt(delta) - b) / (2 * a);
+                x2 = ( -(float)Math.Sqrt(delta) - b) / (2 * a);
+                Console.WriteLine("x1={0}", x1);
+                Console.WriteLine("x2={0}", x2);
+            }
+            else if(delta == 0)
+            {
+                float x;
+                x = -b / (2 * a);
+                Console.WriteLine("x={0}", x);
+            }
+            else
+            {
+                delta = -delta;
+                delta=(float)Math.Sqrt(delta);
+
+                Console.WriteLine("x1 = (-{0}+i*{1}) / 2*{2}", b, delta, a);
+                Console.WriteLine("x2 = (-{0}-i*{1}) / 2*{2}", b, delta, a);
+            }
+        }
+    }
+}
