f8500ca1423a596a60e3a20ffcc3ab15d916d016
dayvey <115526062+dayvey@users.noreply.github.com>
2022-11-30 11:50:11 +0200

    Add files via upload

---
+++ 17/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, m;
+
+            Console.Write("n=");
+            n=int.Parse(Console.ReadLine());
+
+            Console.Write("m=");
+            m = int.Parse(Console.ReadLine());
+
+            int x = n, y = m;
+
+            while (x != y)
+            {
+                if (x < y)
+                    y -= x;
+                else x -= y;
+            }
+            Console.WriteLine("Cel mai mare divizor comun al lui {0} si {1} este {2}", n, m, x);
+
+            int a = n; int b = m; y = m; x = n; 
+
+            while (x!=y)
+            {
+                if (x < y)
+                    x = x + a;
+                else y = y + b;
+            }
+            Console.WriteLine("Cel mai mic multiplu comun al lui {0} si {1} este {2}", n, m, x);
+        }
+    }
+}
---
+++ 18/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace _18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.Write("n=");
+            n=Convert.ToInt32(Console.ReadLine());
+            Console.Write("n=");
+
+            int nr = 0;
+            int d = 2;
+            int c2 = 0;
+
+            while (n>1)
+            {
+                int c = 0;
+                int d2 = d;
+                for (int i = 2; i <= d2 / 2 && c == 0; i++)
+                    if (d2 % i == 0)
+                        c = 1;
+                if (c==0)
+                {
+                    if (c2 != 0)
+                    {
+                        if (nr!=0)
+                            Console.Write("x");
+                    }
+                    else c2 = 1;
+                    nr = 0;
+                    while (n % d == 0)
+                    {
+                        n /= d;
+                        nr++;
+                    }
+                    if (nr != 0)
+                    {
+                        Console.Write("{0}^{1}", d, nr);
+                    }
+                }
+                d++;
+            }
+        }
+    }
+}
