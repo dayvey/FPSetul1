467660f4f0173fb204cc02b527dfe12e9a5457c9
dayvey <115526062+dayvey@users.noreply.github.com>
2022-11-30 11:44:59 +0200

    Add files via upload

---
+++ 7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            Console.Write("a=");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("b=");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            c = a;
+            a = b;
+            b = c;
+
+            Console.WriteLine("a={0} si b={1}", a, b);
+
+        }
+    }
+}
---
+++ 8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            Console.Write("a=");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("b=");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine("a={0} si b={1}", a, b);
+        }
+    }
+}
