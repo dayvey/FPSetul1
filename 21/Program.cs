9ab283bc41c45fff692fb128c04dfe55f491548f
dayvey <115526062+dayvey@users.noreply.github.com>
2022-11-30 11:51:56 +0200

    Add files via upload

---
+++ 21/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            //int n=rnd.Next(1,1025);
+
+            int x = 0;
+
+            Console.WriteLine(n);
+
+            while (x != n)
+            {
+                Console.Write("Numarul este mai mare sau egal decat ");
+                x = Convert.ToInt32(Console.ReadLine());
+                if (x < n) Console.WriteLine("Mai mare!");
+                else if (x > n) Console.WriteLine("Mai mic!");
+                     else Console.WriteLine("Felicitari! Ai ghicit numarul n!");
+            }
+        }
+    }
+}
