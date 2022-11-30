ab6215e09e8eb1c1935e41ac27a7edce32a4495d
dayvey <115526062+dayvey@users.noreply.github.com>
2022-11-30 11:41:50 +0200

    Add files via upload

---
+++ 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, k;
+
+            Console.Write("n=");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("k=");
+            k = Convert.ToInt32(Console.ReadLine());
+
+            if (n % k == 0)
+                Console.WriteLine("n se divide cu k!");
+            else Console.WriteLine("n nu se divide cu k!");
+        }
+    }
+}
---
+++ 4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int y;
+
+            Console.Write("y=");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            if (y % 4 == 0)
+                if (y % 100 != 0 && y % 400 != 0)
+                    Console.WriteLine("y este an bisect!");
+                else if (y % 100 == 0 && y % 400 != 0)
+                        Console.WriteLine("y nu este an bisect!");
+                     else Console.WriteLine("y este an bisect!");
+            else Console.WriteLine("y nu este an bisect!");
+
+        }
+    }
+}
