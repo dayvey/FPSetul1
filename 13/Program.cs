7a03cd95ad6db3836dbc215cbb10fbe5aed3f26f
dayvey <115526062+dayvey@users.noreply.github.com>
2022-11-30 11:48:10 +0200

    Add files via upload

---
+++ 13/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int y1, y2;
+
+            Console.Write("y1=");
+            y1=int.Parse(Console.ReadLine());
+
+            Console.Write("y2=");
+            y2 = int.Parse(Console.ReadLine());
+
+            int c = 0;
+
+            int x = y1;
+
+            int nr = 0;
+
+            while (x+4<=y2)
+            {
+                if (c == 0)
+                {
+                    if (x % 4 == 0)
+                        if (x % 100 != 0 && x % 400 != 0)
+                        {
+                            c = 1;
+                            nr++;
+                        }
+                        else if (x % 100 == 0 && x % 400 != 0)
+                            c = 0;
+                        else
+                        {
+                            c = 1;
+                            nr++;
+                        }
+                    else c = 0;
+                    x++;
+                }
+                else
+                {
+                    nr++;
+                    x += 4;
+                }
+            }
+            Console.WriteLine("Intre {0} si {1} sunt {2} ani bisecti", y1, y2, nr+1);
+        }
+    }
+}
---
+++ 14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, m=0;
+
+            Console.Write("n=");
+            n = int.Parse(Console.ReadLine());
+
+            int l = n;
+
+            while (n>0)
+            {
+                m = m * 10 + n % 10;
+                n /= 10;
+            }
+
+            if (l == m)
+                Console.WriteLine("{0} este palindrom!", l);
+            else Console.WriteLine("{0} nu este palindrom!", l);
+        }
+    }
+}
