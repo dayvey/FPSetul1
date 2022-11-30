81001df977e7eefe5ff92c06926b5f5b7e8adbe9
dayvey <115526062+dayvey@users.noreply.github.com>
2022-11-30 11:51:10 +0200

    Add files via upload

---
+++ 19/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, m;
+
+            Console.Write("n=");
+            n=Convert.ToInt32(Console.ReadLine());
+
+            m = n;
+
+            int x=0, y=-1;
+
+            x = m % 10;
+            m /= 10;
+
+            bool c = true;
+
+            while (m>0 && c==true)
+            {
+                if (m % 10 != x && m % 10 != y)
+                {
+                    if (y == -1)
+                        y = m % 10;
+                    else c = false;
+                }
+                m /= 10;
+            }
+
+            if (c == true) Console.WriteLine("Numarul {0} este formata din 2 cifre care se pot repeta, care sunt {1} si {2}.", n, x, y);
+            else Console.WriteLine("Numarul {0} nu este formata numai din 2 cifre care se pot repeta.", n);
+        }
+    }
+}
---
+++ 20/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace _20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            fractie2();
+        }
+
+        private static void fractie2()
+        {
+            int n, m;
+            Console.Write("m=");
+            m = int.Parse(Console.ReadLine());
+            Console.Write("n=");
+            n = int.Parse(Console.ReadLine());
+            // TODO aducem fractia la forma ireductibila. 
+            int parteInt, parteFract;
+            parteInt = m / n;
+            parteFract = m % n;
+            Console.Write($"{parteInt},");
+            int cifra, rest;
+            List<int> resturi = new List<int>();
+            List<int> cifre = new List<int>();
+            resturi.Add(parteFract);
+            bool periodic = false;
+            do
+            {
+                cifra = parteFract * 10 / n;
+                cifre.Add(cifra);
+                //Console.Write($"{cifra}");
+                rest = parteFract * 10 % n;
+                if (!resturi.Contains(rest))
+                {
+                    resturi.Add(rest);
+                }
+                else
+                {
+                    periodic = true;
+                    break;
+                }
+                parteFract = rest;
+            } while (rest != 0);
+
+            if (!periodic)
+            {
+                foreach (var item in cifre)
+                {
+                    Console.Write(item);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < resturi.Count; i++)
+                {
+                    if (resturi[i] == rest)
+                    {
+                        Console.Write("(");
+                    }
+                    Console.Write(cifre[i]);
+                }
+                Console.WriteLine(")");
+            }
+        }
+    }
+}
